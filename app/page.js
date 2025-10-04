5fdaa7b397497726ba5577a6dc030219a175ef1e
bhavy-sharma <bhavymay18@gmail.com>
2025-10-04 11:47:50 +0530

    Header

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Header from '@/components/Header';
 import WhatWeDo from '../components/WhatWeDo';
 import WhoWeAre from '../components/WhoWeAre';
 
 export default function HomePage() {
   return (
     <main>
+      <Header />
       <WhatWeDo />
       <WhoWeAre />
     </main>
