6648b6ac9a752c7b426dcd91e026728c2fabce69
Sjain0018 <sj5373557@gmail.com>
2025-10-04 16:56:55 +0530

    Fix header layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { AuthProvider } from '@/context/AuthContext';
-import Header from '@/components/Header';
 import './globals.css';
-import Footer from '@/components/Footer';
+import ClientLayout from './ClientLayout';
 
 export const metadata = {
   title: 'Healthwise',
@@ -12,11 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
-          <Header />
-          <main>{children}</main>
-          <Footer />
-        </AuthProvider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
