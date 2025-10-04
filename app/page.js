649a154ef4b5f2d6232b3dfaa89b53d759c99e12
Sjain0018 <sj5373557@gmail.com>
2025-10-04 19:27:51 +0530

    Remove disclamier button

--- app/page.js
+++ app/page.js
@@ -3,14 +3,12 @@ import WhatWeDo from '@/components/WhatWeDo';
 import WhoWeAre from '@/components/WhoWeAre';
 import WowHeroSection from '@/components/WowHeroSection';
 import Footer from '@/components/Footer';
-import DisclaimerButtonSection from '@/components/DisclaimerButtonSection';
 export default function HomePage() {
   return (
     <main>
       {/* <Header /> */}
       <WowHeroSection />
       <WhatWeDo />
-      <DisclaimerButtonSection />
       <WhoWeAre />
       {/* <Footer /> */}
     </main>
