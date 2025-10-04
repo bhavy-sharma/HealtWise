6229e4a8b34a9c3803b768d07976365e791edcd6
Tamanna <tamannatiwari985@gmail.com>
2025-10-04 14:02:46 +0530

    Home

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
 import WhatWeDo from '../components/WhatWeDo';
 import WhoWeAre from '../components/WhoWeAre';
+import Footer from '@/components/Footer';
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Header />
       <WhatWeDo />
       <WhoWeAre />
+      <Footer />
     </main>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
 return (
-    <header className="bg-green-600 text-white flex justify-between items-center px-6 py-4 shadow-md">
+    <header className="bg-[#f0f4ff] text-black flex justify-between items-center px-6 py-4 shadow-md">
     <div className="text-2xl font-bold">Healthwise</div>
 
 
@@ -11,14 +11,14 @@ return (
         <ul className="flex space-x-6">
         <li><a href="#" className="hover:underline">Home</a></li>
         <li><a href="#" className="hover:underline">About</a></li>
-        <li><a href="#" className="hover:underline">Services</a></li>
+        <li><a href="#" className="hover:underline">Diagnoses</a></li>
         <li><a href="#" className="hover:underline">Contact</a></li>
         </ul>
     </nav>
 
     <a
         href="#"
-        className="bg-white text-green-600 px-4 py-2 rounded-lg font-semibold hover:bg-gray-100 transition"
+        className="bg-[#f0f4ff] text-black px-4 py-2 rounded-lg font-semibold hover:bg-gray-100 transition"
     >
         Login
     </a>
