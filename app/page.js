92214f1c7acb7877c013dae5e7f5664fecaea291
bhavy-sharma <bhavymay18@gmail.com>
2025-10-04 11:22:14 +0530

    meet our Team

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 // app/page.js
 import WhatWeDo from '../components/WhatWeDo';
+import WhoWeAre from '../components/WhoWeAre';
 
 export default function HomePage() {
   return (
     <main>
       <WhatWeDo />
+      <WhoWeAre />
     </main>
   );
 }
